79383faa58e3d3d1d4ca18b4841ccf6fa0e67638
shota <shota19830706@gmail.com>
2023-10-04 19:21:49 +0900

    tailwindcssのブラウザ反映確認

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,11 +4,12 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link href="{{ asset('css/output.css') }}" rel="stylesheet">
+    <link href="{{ asset('build/assets/app-650f9e99.css') }}" rel="stylesheet">
+
     <title>MyBBS</title>
 </head>
 <body>
-    <h1>My BBS</h1>
+    <h1 class="font-bold text-red-500">My BBS</h1>
     <ul>
         <li>Title</li>
         <li>Title</li>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: [
+    './resources/**/*.blade.php', // Bladeテンプレートのすべて
+    './resources/**/*.vue', // Vueコンポーネントのすべて
+    './resources/**/*.jsx', // JSXファイルのすべて
+    './resources/**/*.tsx', // TypeScript Reactファイルのすべて
+    './resources/**/*.js', // JavaScriptファイルも含める
+  ],
+  
   theme: {
     extend: {},
   },
