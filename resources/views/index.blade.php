4be50a09ce585e48b12920559cf673bdb09a8e24
shota <shota19830706@gmail.com>
2023-10-05 07:42:17 +0900

    @viteをLaravelのデフォルトのbladeテンプレートに挿入

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -7,9 +7,10 @@
     <link href="{{ asset('build/assets/app-650f9e99.css') }}" rel="stylesheet">
 
     <title>MyBBS</title>
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 <body>
-    <h1 class="font-bold text-red-500">My BBS</h1>
+    <h1 class="font-bold text-blue-500">My BBS</h1>
     <ul>
         <li>Title</li>
         <li>Title</li>
