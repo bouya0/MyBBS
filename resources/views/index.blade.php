9f127a2d315f9b8f31ce84a9f1c59d8a9a9d4d1a
shota <shota19830706@gmail.com>
2023-10-06 06:00:13 +0900

    レイアウト調整

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -9,12 +9,14 @@
     <title>MyBBS</title>
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
-<body>
-    <h1 class="font-bold text-blue-500">My BBS</h1>
-    <ul>
-        <li>Title</li>
-        <li>Title</li>
-        <li>Title</li>
-    </ul>
+<body class="bg-gray-100">
+    <div class="container mx-auto max-w-screen-md p-10 mt-10 bg-white rounded shadow-lg">
+        <h1 class="text-2xl font-bold border-b border-black pb-2 mb-4">My BBS</h1>
+        <ul class="space-y-2">
+            <li class="text-lg ml-12 leading-6 mb-2">Title</li>
+            <li class="text-lg ml-12 leading-6 mb-2">Title</li>
+            <li class="text-lg ml-12 leading-6 mb-2">Title</li>
+        </ul>
+    </div>
 </body>
 </html>
