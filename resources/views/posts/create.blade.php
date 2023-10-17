725d59ea958646966b3f1f58d768df68b45c46bb
shota <shota19830706@gmail.com>
2023-10-18 05:51:15 +0900

    Embed token in authentication process and refactoring the styling

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -12,14 +12,18 @@
     </h1>
 
     <form method="post" action="">
+        @csrf
+
         <label>
             Title
-            <input class="border-2 border-black rounded focus:border-blue-500" type="text" name="title">
+            <input class="mb-4 w-full p-4 border-2 border-black rounded focus:border-blue-500" type="text" name="title">
         </label>
         <label>
             Body
-            <textarea class="border-2 border-black rounded focus:border-blue-500" name="body"></textarea>
+            <textarea class="mb-4 h-40 w-full p-4 border-2 border-black rounded focus:border-blue-500" name="body"></textarea>
         </label>
-        <button class="bg-gray-300 hover:bg-gray-200 text-black rounded px-4 py-1 border-2 border-black">Add</button>
+        <div class="text-right">
+            <button class="bg-gray-300 hover:bg-gray-200 text-black rounded px-4 py-1 border-2 border-black">Add</button>
+        </div>
     </form>
 </x-layout>
