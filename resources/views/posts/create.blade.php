be3d127ba321726b3f3b8e698e20421b7f469a0f
shota <shota19830706@gmail.com>
2023-10-17 21:55:18 +0900

    Refactoring the Styling

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -10,4 +10,16 @@
     <h1 class="text-3xl font-extrabold pb-4 mb-6 border-b-2 border-indigo-600">
         Add New Post
     </h1>
+
+    <form method="post" action="">
+        <label>
+            Title
+            <input class="border-2 border-black rounded focus:border-blue-500" type="text" name="title">
+        </label>
+        <label>
+            Body
+            <textarea class="border-2 border-black rounded focus:border-blue-500" name="body"></textarea>
+        </label>
+        <button class="bg-gray-300 hover:bg-gray-200 text-black rounded px-4 py-1 border-2 border-black">Add</button>
+    </form>
 </x-layout>
